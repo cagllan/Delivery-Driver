a5b272a15f1b3b4bd3f177a230bd39e4448723ca
Caglan Atay <caglanatay@windowslive.com>
2021-11-07 19:14:30 +0300

    Added keyboard keys for routing.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -17,7 +17,9 @@ public class Driver : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    transform.Rotate(0, 0, steerSpeed);
-    transform.Translate(0, moveSpeed, 0);
+    float steerAmount = steerSpeed * Input.GetAxis("Horizontal");
+    float moveAmount = moveSpeed * Input.GetAxis("Vertical");
+    transform.Rotate(0, 0, -steerAmount);
+    transform.Translate(0, moveAmount, 0);
   }
 }
